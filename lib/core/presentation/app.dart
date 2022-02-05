11e00926c8bfc814f03a01a4d00da85d31008ad4
halilyaman <halilymn03@gmail.com>
2022-02-05 23:34:09 +0300

    Show device preview only in debug mode

--- lib/core/presentation/app.dart
+++ lib/core/presentation/app.dart
@@ -1,3 +1,5 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
@@ -5,6 +7,11 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    return DevicePreview(
+      enabled: kDebugMode,
+      builder: (_) => const MaterialApp(
+        useInheritedMediaQuery: true,
+      ),
+    );
   }
 }
